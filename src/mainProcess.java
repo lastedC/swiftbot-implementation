8575d7e0e752f482660240e4bebc5730ed5a354c
lasted <shihabmohamed04@gmail.com>
2024-02-29 12:58:49 +0000

    changed class and method

--- src/mainProcess.java
+++ src/mainProcess.java
@@ -7,11 +7,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import swiftbot.*;
 
-public class main {
+public class mainProcess {
 
     static SwiftBotAPI swiftBot;
 
-    public static void start(String[] args) throws InterruptedException, IOException {
+    public static void main(String[] args) throws InterruptedException, IOException {
 
         try {
             swiftBot = new SwiftBotAPI();
